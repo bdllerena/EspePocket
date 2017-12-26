2d62408e03359ba85f7c7c6073aaf85cf00e9627
Alejandro <Alejo19@users.noreply.github.com>
2017-12-26 09:59:25 -0500

    Unit Test - Majors Uri

---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,436 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EspePocket.Models;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestUriSistemas()
+        {
+            PersonaWebView valorEsperado = new PersonaWebView
+            {
+                Uri = "https://goo.gl/RRfE18"
+            };
+            var varEsperado = valorEsperado.Uri;
+            PersonaWebView valorActual = new PersonaWebView
+            {
+                Uri = "https://goo.gl/RRfE18"
+            };
+            var varActual = valorActual.Uri;
+
+            Assert.AreEqual(varActual, varEsperado);
+        }
+
+        [TestMethod]
+        public void TestUriMecatronica()
+        {
+            PersonaWebView valorEsperado = new PersonaWebView
+            {
+                Uri = "https://goo.gl/5oyVqR"
+            };
+            var varEsperado = valorEsperado.Uri;
+            PersonaWebView valorActual = new PersonaWebView
+            {
+                Uri = "https://goo.gl/5oyVqR"
+            };
+            var varActual = valorActual.Uri;
+
+            Assert.AreEqual(varActual, varEsperado);
+        }
+
+        [TestMethod]
+        public void TestUriBio()
+        {
+            PersonaWebView valorEsperado = new PersonaWebView
+            {
+                Uri = "https://goo.gl/VFJC1c"
+            };
+            var varEsperado = valorEsperado.Uri;
+            PersonaWebView valorActual = new PersonaWebView
+            {
+                Uri = "https://goo.gl/VFJC1c"
+            };
+            var varActual = valorActual.Uri;
+
+            Assert.AreEqual(varActual, varEsperado);
+        }
+
+        [TestMethod]
+        public void TestUriAutomotriz()
+        {
+            PersonaWebView valorEsperado = new PersonaWebView
+            {
+                Uri = "https://goo.gl/qbvDu3"
+            };
+            var varEsperado = valorEsperado.Uri;
+            PersonaWebView valorActual = new PersonaWebView
+            {
+                Uri = "https://goo.gl/qbvDu3"
+            };
+            var varActual = valorActual.Uri;
+
+            Assert.AreEqual(varActual, varEsperado);
+        }
+
+        [TestMethod]
+        public void TestUriSoftware()
+        {
+            PersonaWebView valorEsperado = new PersonaWebView
+            {
+                Uri = "https://goo.gl/k3c88N"
+            };
+            var varEsperado = valorEsperado.Uri;
+            PersonaWebView valorActual = new PersonaWebView
+            {
+                Uri = "https://goo.gl/k3c88N"
+            };
+            var varActual = valorActual.Uri;
+
+            Assert.AreEqual(varActual, varEsperado);
+        }
+
+        [TestMethod]
+        public void TestUriMecanica()
+        {
+            PersonaWebView valorEsperado = new PersonaWebView
+            {
+                Uri = "https://goo.gl/zoSmRY"
+            };
+            var varEsperado = valorEsperado.Uri;
+            PersonaWebView valorActual = new PersonaWebView
+            {
+                Uri = "https://goo.gl/zoSmRY"
+            };
+            var varActual = valorActual.Uri;
+
+            Assert.AreEqual(varActual, varEsperado);
+        }
+
+        [TestMethod]
+        public void TestUriCivil()
+        {
+            PersonaWebView valorEsperado = new PersonaWebView
+            {
+                Uri = "https://goo.gl/XWgmHb"
+            };
+            var varEsperado = valorEsperado.Uri;
+            PersonaWebView valorActual = new PersonaWebView
+            {
+                Uri = "https://goo.gl/XWgmHb"
+            };
+            var varActual = valorActual.Uri;
+
+            Assert.AreEqual(varActual, varEsperado);
+        }
+
+        [TestMethod]
+        public void TestUriGeo()
+        {
+            PersonaWebView valorEsperado = new PersonaWebView
+            {
+                Uri = "https://goo.gl/ufvjkv"
+            };
+            var varEsperado = valorEsperado.Uri;
+            PersonaWebView valorActual = new PersonaWebView
+            {
+                Uri = "https://goo.gl/ufvjkv"
+            };
+            var varActual = valorActual.Uri;
+
+            Assert.AreEqual(varActual, varEsperado);
+        }
+
+        [TestMethod]
+        public void TestUriAutomatizacion()
+        {
+            PersonaWebView valorEsperado = new PersonaWebView
+            {
+                Uri = "https://goo.gl/VjfxRc"
+            };
+            var varEsperado = valorEsperado.Uri;
+            PersonaWebView valorActual = new PersonaWebView
+            {
+                Uri = "https://goo.gl/VjfxRc"
+            };
+            var varActual = valorActual.Uri;
+
+            Assert.AreEqual(varActual, varEsperado);
+        }
+
+        [TestMethod]
+        public void TestUriTelecomunicaciones()
+        {
+            PersonaWebView valorEsperado = new PersonaWebView
+            {
+                Uri = "https://goo.gl/XVAoti"
+            };
+            var varEsperado = valorEsperado.Uri;
+            PersonaWebView valorActual = new PersonaWebView
+            {
+                Uri = "https://goo.gl/XVAoti"
+            };
+            var varActual = valorActual.Uri;
+
+            Assert.AreEqual(varActual, varEsperado);
+        }
+
+        [TestMethod]
+        public void TestUriIasa()
+        {
+            PersonaWebView valorEsperado = new PersonaWebView
+            {
+                Uri = "https://goo.gl/Vrg2hM"
+            };
+            var varEsperado = valorEsperado.Uri;
+            PersonaWebView valorActual = new PersonaWebView
+            {
+                Uri = "https://goo.gl/Vrg2hM"
+            };
+            var varActual = valorActual.Uri;
+
+            Assert.AreEqual(varActual, varEsperado);
+        }
+
+        [TestMethod]
+        public void TestUriMecatronic()
+        {
+            PersonaWebView valorEsperado = new PersonaWebView
+            {
+                Uri = "https://goo.gl/J773hS"
+            };
+            var varEsperado = valorEsperado.Uri;
+            PersonaWebView valorActual = new PersonaWebView
+            {
+                Uri = "https://goo.gl/J773hS"
+            };
+            var varActual = valorActual.Uri;
+
+            Assert.AreEqual(varActual, varEsperado);
+        }
+
+        [TestMethod]
+        public void TestUriElectrom()
+        {
+            PersonaWebView valorEsperado = new PersonaWebView
+            {
+                Uri = "https://goo.gl/1BtdeH"
+            };
+            var varEsperado = valorEsperado.Uri;
+            PersonaWebView valorActual = new PersonaWebView
+            {
+                Uri = "https://goo.gl/1BtdeH"
+            };
+            var varActual = valorActual.Uri;
+
+            Assert.AreEqual(varActual, varEsperado);
+        }
+
+        [TestMethod]
+        public void TestUriTecelec()
+        {
+            PersonaWebView valorEsperado = new PersonaWebView
+            {
+                Uri = "https://goo.gl/KMWgqa"
+            };
+            var varEsperado = valorEsperado.Uri;
+            PersonaWebView valorActual = new PersonaWebView
+            {
+                Uri = "https://goo.gl/KMWgqa"
+            };
+            var varActual = valorActual.Uri;
+
+            Assert.AreEqual(varActual, varEsperado);
+        }
+
+        [TestMethod]
+        public void TestUriFinanzas()
+        {
+            PersonaWebView valorEsperado = new PersonaWebView
+            {
+                Uri = "https://goo.gl/GgAqYc"
+            };
+            var varEsperado = valorEsperado.Uri;
+            PersonaWebView valorActual = new PersonaWebView
+            {
+                Uri = "https://goo.gl/GgAqYc"
+            };
+            var varActual = valorActual.Uri;
+
+            Assert.AreEqual(varActual, varEsperado);
+        }
+
+        [TestMethod]
+        public void TestUriComercial()
+        {
+            PersonaWebView valorEsperado = new PersonaWebView
+            {
+                Uri = "https://goo.gl/BWQ1vM"
+            };
+            var varEsperado = valorEsperado.Uri;
+            PersonaWebView valorActual = new PersonaWebView
+            {
+                Uri = "https://goo.gl/BWQ1vM"
+            };
+            var varActual = valorActual.Uri;
+
+            Assert.AreEqual(varActual, varEsperado);
+        }
+
+        [TestMethod]
+        public void TestUriTuristica()
+        {
+            PersonaWebView valorEsperado = new PersonaWebView
+            {
+                Uri = "https://goo.gl/us5ngn"
+            };
+            var varEsperado = valorEsperado.Uri;
+            PersonaWebView valorActual = new PersonaWebView
+            {
+                Uri = "https://goo.gl/us5ngn"
+            };
+            var varActual = valorActual.Uri;
+
+            Assert.AreEqual(varActual, varEsperado);
+        }
+
+        [TestMethod]
+        public void TestUriPetro()
+        {
+            PersonaWebView valorEsperado = new PersonaWebView
+            {
+                Uri = "https://goo.gl/KSLv1V"
+            };
+            var varEsperado = valorEsperado.Uri;
+            PersonaWebView valorActual = new PersonaWebView
+            {
+                Uri = "https://goo.gl/KSLv1V"
+            };
+            var varActual = valorActual.Uri;
+
+            Assert.AreEqual(varActual, varEsperado);
+        }
+
+        [TestMethod]
+        public void TestUriCeni()
+        {
+            PersonaWebView valorEsperado = new PersonaWebView
+            {
+                Uri = "https://goo.gl/w3VPHj"
+            };
+            var varEsperado = valorEsperado.Uri;
+            PersonaWebView valorActual = new PersonaWebView
+            {
+                Uri = "https://goo.gl/w3VPHj"
+            };
+            var varActual = valorActual.Uri;
+
+            Assert.AreEqual(varActual, varEsperado);
+        }
+
+        [TestMethod]
+        public void TestUriEducacion()
+        {
+            PersonaWebView valorEsperado = new PersonaWebView
+            {
+                Uri = "https://goo.gl/FeSQuH"
+            };
+            var varEsperado = valorEsperado.Uri;
+            PersonaWebView valorActual = new PersonaWebView
+            {
+                Uri = "https://goo.gl/FeSQuH"
+            };
+            var varActual = valorActual.Uri;
+
+            Assert.AreEqual(varActual, varEsperado);
+        }
+
+        [TestMethod]
+        public void TestUriFinanza()
+        {
+            PersonaWebView valorEsperado = new PersonaWebView
+            {
+                Uri = "https://goo.gl/Umz8bn"
+            };
+            var varEsperado = valorEsperado.Uri;
+            PersonaWebView valorActual = new PersonaWebView
+            {
+                Uri = "https://goo.gl/Umz8bn"
+            };
+            var varActual = valorActual.Uri;
+
+            Assert.AreEqual(varActual, varEsperado);
+        }
+
+        [TestMethod]
+        public void TestUriFisica()
+        {
+            PersonaWebView valorEsperado = new PersonaWebView
+            {
+                Uri = "https://goo.gl/kYDWXn"
+            };
+            var varEsperado = valorEsperado.Uri;
+            PersonaWebView valorActual = new PersonaWebView
+            {
+                Uri = "https://goo.gl/kYDWXn"
+            };
+            var varActual = valorActual.Uri;
+
+            Assert.AreEqual(varActual, varEsperado);
+        }
+
+        [TestMethod]
+        public void TestUriAviones()
+        {
+            PersonaWebView valorEsperado = new PersonaWebView
+            {
+                Uri = "https://goo.gl/xGjHMF"
+            };
+            var varEsperado = valorEsperado.Uri;
+            PersonaWebView valorActual = new PersonaWebView
+            {
+                Uri = "https://goo.gl/xGjHMF"
+            };
+            var varActual = valorActual.Uri;
+
+            Assert.AreEqual(varActual, varEsperado);
+        }
+
+        [TestMethod]
+        public void TestUriMotores()
+        {
+            PersonaWebView valorEsperado = new PersonaWebView
+            {
+                Uri = "https://goo.gl/hNu7Ae"
+            };
+            var varEsperado = valorEsperado.Uri;
+            PersonaWebView valorActual = new PersonaWebView
+            {
+                Uri = "https://goo.gl/hNu7Ae"
+            };
+            var varActual = valorActual.Uri;
+
+            Assert.AreEqual(varActual, varEsperado);
+        }
+
+        [TestMethod]
+        public void TestUriMerca()
+        {
+            PersonaWebView valorEsperado = new PersonaWebView
+            {
+                Uri = "https://goo.gl/qbyh8x"
+            };
+            var varEsperado = valorEsperado.Uri;
+            PersonaWebView valorActual = new PersonaWebView
+            {
+                Uri = "https://goo.gl/qbyh8x"
+            };
+            var varActual = valorActual.Uri;
+
+            Assert.AreEqual(varActual, varEsperado);
+        }
+
+    }
+}
