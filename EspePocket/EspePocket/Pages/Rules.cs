b0c0b24ea925251e492c043d396a696c598f9a7f
dapalacios3 <dapalacios3@espe.edu.ec>
2018-01-29 00:23:44 -0500

    Standarize the comments to english
    Add Comments in classes Careers, FAQ,  and others.

--- EspePocket/EspePocket/Pages/Rules.cs
+++ EspePocket/EspePocket/Pages/Rules.cs
@@ -6,6 +6,9 @@ namespace EspePocket.Pages
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Rules : ContentPage
     {
+        /// <summary>
+        /// this class redirect to the ESPE Students Regulations and ESPE License plates Regualtions
+        /// </summary>
         public Rules()
         {
             Title = "Reglamentos";
--- EspePocket/EspePocket/Pages/careers.xaml.cs
+++ EspePocket/EspePocket/Pages/careers.xaml.cs
@@ -9,17 +9,18 @@ namespace EspePocket.Pages
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Careers : ContentPage
     {
-	/// <summary>
-        /// Esta clase se encarga de redireccionar a las carreras comprendidas en diferentes campos.
+        /// <summary>
+        /// this class contains the methods to the information of the different existing careers.
         /// </summary>
         public Careers()
         {
             InitializeComponent();
 
         }
-	/// <summary>
-        /// Este metodo de la clase Carrers redirecciona a la carrera de Sistemas
-	/// La variable browser contiene la uri como dato quemado.
+        /// <summary>
+        /// this method redirect to the systems engineering repository.
+        /// <param name="sender">contains a reference to the control/object that raised the event</param>
+        /// <param name="args">is the base class to contain the events thrown by the control</param>
         /// </summary>
         public void Sistemas(object sender, EventArgs args)
         {
@@ -30,9 +31,10 @@ namespace EspePocket.Pages
             Content = browser;
 
         }
-	/// <summary>
-        /// Este metodo de la clase Carrers redirecciona a la carrera de Mecatronica
-	/// La variable browser contiene la uri como dato quemado.
+        /// <summary>
+        /// this method redirect to the Mecatronic enfineering repository.
+        /// <param name="sender">contains a reference to the control/object that raised the event</param>
+        /// <param name="args">is the base class to contain the events thrown by the control</param>
         /// </summary>
         public void Mecatronica(object sender, EventArgs args)
         {
@@ -43,9 +45,10 @@ namespace EspePocket.Pages
             Content = browser;
 
         }
-	/// <summary>
-        /// Este metodo de la clase Carrers redirecciona a la carrera de Biotecnologia
-	/// La variable browser contiene la uri como dato quemado.
+        /// <summary>
+        /// this method redirect to the Biological engineering repository.
+        /// <param name="sender">contains a reference to the control/object that raised the event</param>
+        /// <param name="args">is the base class to contain the events thrown by the control</param>
         /// </summary>
         public void Bio(object sender, EventArgs args)
         {
@@ -56,9 +59,10 @@ namespace EspePocket.Pages
             Content = browser;
 
         }
-	/// <summary>
-        /// Este metodo de la clase Carrers redirecciona a la carrera de Ingenieria Automotriz
-	/// La variable browser contiene la uri como dato quemado.
+        /// <summary>
+        /// this method redirect to the Automotive engineering repository.
+        /// <param name="sender">contains a reference to the control/object that raised the event</param>
+        /// <param name="args">is the base class to contain the events thrown by the control</param>
         /// </summary>
         public void Automotriz(object sender, EventArgs args)
         {
@@ -69,9 +73,10 @@ namespace EspePocket.Pages
             Content = browser;
 
         }
-	/// <summary>
-        /// Este metodo de la clase Carrers redirecciona a la carrera de Ingenieria de Software
-	/// La variable browser contiene la uri como dato quemado.
+        /// <summary>
+        /// this method redirect to the Software engineering repository.
+        /// <param name="sender">contains a reference to the control/object that raised the event</param>
+        /// <param name="args">is the base class to contain the events thrown by the control</param>
         /// </summary>
         public void Software(object sender, EventArgs args)
         {
@@ -82,9 +87,10 @@ namespace EspePocket.Pages
             Content = browser;
 
         }
-	/// <summary>
-        /// Este metodo de la clase Carrers redirecciona a la carrera de Mecanica
-	/// La variable browser contiene la uri como dato quemado.
+        /// <summary>
+        /// this method redirect to the Mecanics engineering repository.
+        /// <param name="sender">contains a reference to the control/object that raised the event</param>
+        /// <param name="args">is the base class to contain the events thrown by the control</param>
         /// </summary>
         public void Mecanica(object sender, EventArgs args)
         {
@@ -95,9 +101,10 @@ namespace EspePocket.Pages
             Content = browser;
 
         }
-	/// <summary>
-        /// Este metodo de la clase Carrers redirecciona a la carrera de Ingenieria Civil
-	/// La variable browser contiene la uri como dato quemado.
+        /// <summary>
+        /// this method redirect to the Civil engineering repository.
+        /// <param name="sender">contains a reference to the control/object that raised the event</param>
+        /// <param name="args">is the base class to contain the events thrown by the control</param>
         /// </summary>
         public void Civil(object sender, EventArgs args)
         {
@@ -108,9 +115,10 @@ namespace EspePocket.Pages
             Content = browser;
 
         }
-	/// <summary>
-        /// Este metodo de la clase Carrers redirecciona a la carrera de Ingenieria Geografica
-	/// La variable browser contiene la uri como dato quemado.
+        /// <summary>
+        /// this method redirect to the Geographic engineering repository.
+        /// <param name="sender">contains a reference to the control/object that raised the event</param>
+        /// <param name="args">is the base class to contain the events thrown by the control</param>
         /// </summary>
         public void Geo(object sender, EventArgs args)
         {
@@ -121,9 +129,10 @@ namespace EspePocket.Pages
             Content = browser;
 
         }
-	/// <summary>
-        /// Este metodo de la clase Carrers redirecciona a la carrera de Automatizacion
-	/// La variable browser contiene la uri como dato quemado.
+        /// <summary>
+        /// this method redirect to the Automation and Control repository.
+        /// <param name="sender">contains a reference to the control/object that raised the event</param>
+        /// <param name="args">is the base class to contain the events thrown by the control</param>
         /// </summary>
         public void Automatizacion(object sender, EventArgs args)
         {
@@ -134,9 +143,10 @@ namespace EspePocket.Pages
             Content = browser;
 
         }
-	/// <summary>
-        /// Este metodo de la clase Carrers redirecciona a la carrera de Telecomunicaciones
-	/// La variable browser contiene la uri como dato quemado.
+        /// <summary>
+        /// this method redirect to the Telecommunications repository.
+        /// <param name="sender">contains a reference to the control/object that raised the event</param>
+        /// <param name="args">is the base class to contain the events thrown by the control</param>
         /// </summary>
         public void Telecomunicaciones(object sender, EventArgs args)
         {
@@ -147,9 +157,10 @@ namespace EspePocket.Pages
             Content = browser;
 
         }
-	/// <summary>
-        /// Este metodo de la clase Carrers redirecciona a la carrera de Iasa
-	/// La variable browser contiene la uri como dato quemado.
+        /// <summary>
+        /// this method redirect to the IASA repository.
+        /// <param name="sender">contains a reference to the control/object that raised the event</param>
+        /// <param name="args">is the base class to contain the events thrown by the control</param>
         /// </summary>
         public void Iasa(object sender, EventArgs args)
         {
@@ -160,9 +171,10 @@ namespace EspePocket.Pages
             Content = browser;
 
         }
-	/// <summary>
-        /// Este metodo de la clase Carrers redirecciona a la carrera de Mecatronica
-	/// La variable browser contiene la uri como dato quemado.
+        /// <summary>
+        /// this method redirect to the Mecatronic engineering repository.
+        /// <param name="sender">contains a reference to the control/object that raised the event</param>
+        /// <param name="args">is the base class to contain the events thrown by the control</param>
         /// </summary>
         public void Mecatronic(object sender, EventArgs args)
         {
@@ -173,9 +185,10 @@ namespace EspePocket.Pages
             Content = browser;
 
         }
-	/// <summary>
-        /// Este metodo de la clase Carrers redirecciona a la carrera de Electronica
-	/// La variable browser contiene la uri como dato quemado.
+        /// <summary>
+        /// this method redirect to the Electromechanics repository.
+        /// <param name="sender">contains a reference to the control/object that raised the event</param>
+        /// <param name="args">is the base class to contain the events thrown by the control</param>
         /// </summary>
         public void Electrom(object sender, EventArgs args)
         {
@@ -186,9 +199,10 @@ namespace EspePocket.Pages
             Content = browser;
 
         }
-	/// <summary>
-        /// Este metodo de la clase Carrers redirecciona a la carrera de Tecelec
-	/// La variable browser contiene la uri como dato quemado.
+        /// <summary>
+        /// this method redirect to the Instrumentation Electronics repository.
+        /// <param name="sender">contains a reference to the control/object that raised the event</param>
+        /// <param name="args">is the base class to contain the events thrown by the control</param>
         /// </summary>
         public void Tecelec(object sender, EventArgs args)
         {
@@ -199,9 +213,10 @@ namespace EspePocket.Pages
             Content = browser;
 
         }
-	/// <summary>
-        /// Este metodo de la clase Carrers redirecciona a la carrera de Finanzas
-	/// La variable browser contiene la uri como dato quemado.
+        /// <summary>
+        /// this method redirect to the Finance and Audit repository.
+        /// <param name="sender">contains a reference to the control/object that raised the event</param>
+        /// <param name="args">is the base class to contain the events thrown by the control</param>
         /// </summary>
         public void Finanzas(object sender, EventArgs args)
         {
@@ -212,9 +227,10 @@ namespace EspePocket.Pages
             Content = browser;
 
         }
-	/// <summary>
-        /// Este metodo de la clase Carrers redirecciona a la carrera de Comercial
-	/// La variable browser contiene la uri como dato quemado.
+        /// <summary>
+        /// this method redirect to the Commercial engineering repository.
+        /// <param name="sender">contains a reference to the control/object that raised the event</param>
+        /// <param name="args">is the base class to contain the events thrown by the control</param>
         /// </summary>
         public void Comercial(object sender, EventArgs args)
         {
@@ -225,9 +241,10 @@ namespace EspePocket.Pages
             Content = browser;
 
         }
-	/// <summary>
-        /// Este metodo de la clase Carrers redirecciona a la carrera de Turismo
-	/// La variable browser contiene la uri como dato quemado.
+        /// <summary>
+        /// this method redirect to the Hotel Tourism Management repository.
+        /// <param name="sender">contains a reference to the control/object that raised the event</param>
+        /// <param name="args">is the base class to contain the events thrown by the control</param>
         /// </summary>
         public void Turistica(object sender, EventArgs args)
         {
@@ -238,6 +255,11 @@ namespace EspePocket.Pages
             Content = browser;
 
         }
+        /// <summary>
+        /// this method redirect to the systems Petrochemistry repository.
+        /// <param name="sender">contains a reference to the control/object that raised the event</param>
+        /// <param name="args">is the base class to contain the events thrown by the control</param>
+        /// </summary>
         public void Petro(object sender, EventArgs args)
         {
             var browser = new PersonWebView
@@ -247,6 +269,11 @@ namespace EspePocket.Pages
             Content = browser;
 
         }
+        /// <summary>
+        /// this method redirect to the systems Foreign Trade repository.
+        /// <param name="sender">contains a reference to the control/object that raised the event</param>
+        /// <param name="args">is the base class to contain the events thrown by the control</param>
+        /// </summary>
         public void Ceni(object sender, EventArgs args)
         {
             var browser = new PersonWebView
@@ -256,9 +283,10 @@ namespace EspePocket.Pages
             Content = browser;
 
         }
-	/// <summary>
-        /// Este metodo de la clase Carrers redirecciona a la carrera de Educacion
-	/// La variable browser contiene la uri como dato quemado.
+        /// <summary>
+        /// this method redirect to the Initial Education repository.
+        /// <param name="sender">contains a reference to the control/object that raised the event</param>
+        /// <param name="args">is the base class to contain the events thrown by the control</param>
         /// </summary>
         public void Educacion(object sender, EventArgs args)
         {
@@ -269,9 +297,10 @@ namespace EspePocket.Pages
             Content = browser;
 
         }
-	/// <summary>
-        /// Este metodo de la clase Carrers redirecciona a la carrera de Finanza
-	/// La variable browser contiene la uri como dato quemado.
+        /// <summary>
+        /// this method redirect to the systems Finance repository.
+        /// <param name="sender">contains a reference to the control/object that raised the event</param>
+        /// <param name="args">is the base class to contain the events thrown by the control</param>
         /// </summary>
         public void Finanza(object sender, EventArgs args)
         {
@@ -282,9 +311,10 @@ namespace EspePocket.Pages
             Content = browser;
 
         }
-	/// <summary>
-        /// Este metodo de la clase Carrers redirecciona a la carrera de Actividad Fisica
-	/// La variable browser contiene la uri como dato quemado.
+        /// <summary>
+        /// this method redirect to the Physics & Sports repository.
+        /// <param name="sender">contains a reference to the control/object that raised the event</param>
+        /// <param name="args">is the base class to contain the events thrown by the control</param>
         /// </summary>
         public void Fisica(object sender, EventArgs args)
         {
@@ -295,9 +325,10 @@ namespace EspePocket.Pages
             Content = browser;
 
         }
-	/// <summary>
-        /// Este metodo de la clase Carrers redirecciona a la carrera de Aviones
-	/// La variable browser contiene la uri como dato quemado.
+        /// <summary>
+        /// this method redirect to the systems Aeronautical Mechanics repository.
+        /// <param name="sender">contains a reference to the control/object that raised the event</param>
+        /// <param name="args">is the base class to contain the events thrown by the control</param>
         /// </summary>
         public void Aviones(object sender, EventArgs args)
         {
@@ -308,9 +339,10 @@ namespace EspePocket.Pages
             Content = browser;
 
         }
-	/// <summary>
-        /// Este metodo de la clase Carrers redirecciona a la carrera de Motores
-	/// La variable browser contiene la uri como dato quemado.
+        /// <summary>
+        /// this method redirect to the systems Aeronautics Motors repository.
+        /// <param name="sender">contains a reference to the control/object that raised the event</param>
+        /// <param name="args">is the base class to contain the events thrown by the control</param>
         /// </summary>
         public void Motores(object sender, EventArgs args)
         {
@@ -321,9 +353,10 @@ namespace EspePocket.Pages
             Content = browser;
 
         }
-	/// <summary>
-        /// Este metodo de la clase Carrers redirecciona a la carrera de Mercadotecnia
-	/// La variable browser contiene la uri como dato quemado.
+        /// <summary>
+        /// this method redirect to the Marketing repository.
+        /// <param name="sender">contains a reference to the control/object that raised the event</param>
+        /// <param name="args">is the base class to contain the events thrown by the control</param>
         /// </summary>
         public void Merca(object sender, EventArgs args)
         {
